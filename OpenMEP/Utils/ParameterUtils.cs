cc49483be2064758753caa8e8f4e1fb4e517f7af
chuongmep <chuongpqvn@gmail.com>
2023-01-24 13:53:55 +0700

    Create ParameterUtils.cs

---
+++ OpenMEP/Utils/ParameterUtils.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Utils;
+
+public class ParameterUtils
+{
+    private ParameterUtils()
+    {
+        
+    }
+    
+    /// <summary>
+    ///  Gets the BuiltInParameter value corresponding to built-in parameter identified by the given ForgeTypeId.
+    /// </summary>
+    /// <returns>The BuiltInParameter value corresponding to the given parameter identifier.</returns>
+    /// <param name="forgeTypeId">The parameter identifier.</param>
+    public static dynamic GetBuiltInParameter(ForgeTypeId forgeTypeId)
+    {
+        BuiltInParameter builtInParameter = Autodesk.Revit.DB.ParameterUtils.GetBuiltInParameter(forgeTypeId);
+        return builtInParameter;
+    }
+}
