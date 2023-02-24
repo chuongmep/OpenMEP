e42a2ecca0de57831cbf6e86901dd2d3aa7157a5
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-02-24 20:55:31 +0800

    add point is in polygon

--- OpenMEPSandbox/Geometry/Point.cs
+++ OpenMEPSandbox/Geometry/Point.cs
@@ -9,7 +9,6 @@ public class Point
 {
     private Point()
     {
-        
     }
 
     /// <summary>
@@ -20,35 +19,36 @@ public class Point
     /// <returns name="point">new point projected on plane</returns>
     public static Autodesk.DesignScript.Geometry.Point ProjectOntoPlane(
         Autodesk.DesignScript.Geometry.Point point,
-        Autodesk.DesignScript.Geometry.Vector planeNormal )
+        Autodesk.DesignScript.Geometry.Vector planeNormal)
     {
         double a = planeNormal.X;
         double b = planeNormal.Y;
         double c = planeNormal.Z;
 
-        double dx = ( b * b + c * c ) * point.X - ( a * b ) * point.Y - ( a * c ) * point.Z;
-        double dy = -( b * a ) * point.X + ( a * a + c * c ) * point.Y - ( b * c ) * point.Z;
-        double dz = -( c * a ) * point.X - ( c * b ) * point.Y + ( a * a + b * b ) * point.Z;
-        return Autodesk.DesignScript.Geometry.Point.ByCoordinates( dx, dy, dz );
+        double dx = (b * b + c * c) * point.X - (a * b) * point.Y - (a * c) * point.Z;
+        double dy = -(b * a) * point.X + (a * a + c * c) * point.Y - (b * c) * point.Z;
+        double dz = -(c * a) * point.X - (c * b) * point.Y + (a * a + b * b) * point.Z;
+        return Autodesk.DesignScript.Geometry.Point.ByCoordinates(dx, dy, dz);
     }
-    
+
     /// <summary>
     /// Get the centroid of a list of points
     /// </summary>
     /// <param name="points">list of points</param>
     /// <returns name="point">centroid</returns>
-    public static Autodesk.DesignScript.Geometry.Point Centroid( List<Autodesk.DesignScript.Geometry.Point> points )
+    public static Autodesk.DesignScript.Geometry.Point Centroid(List<Autodesk.DesignScript.Geometry.Point> points)
     {
         double x = 0;
         double y = 0;
         double z = 0;
-        foreach ( Autodesk.DesignScript.Geometry.Point point in points )
+        foreach (Autodesk.DesignScript.Geometry.Point point in points)
         {
             x += point.X;
             y += point.Y;
             z += point.Z;
         }
-        return Autodesk.DesignScript.Geometry.Point.ByCoordinates( x / points.Count, y / points.Count, z / points.Count );
+
+        return Autodesk.DesignScript.Geometry.Point.ByCoordinates(x / points.Count, y / points.Count, z / points.Count);
     }
 
     /// <summary>Test whether a point lies on a line.</summary>
@@ -57,9 +57,10 @@ public class Point
     /// <param name="tolerance">Default is use 1e-6</param>
     /// <returns name="bool">Returns true if point is on line.</returns>
     [NodeCategory("Query")]
-    public static bool IsOnLine(Autodesk.DesignScript.Geometry.Point point,Autodesk.DesignScript.Geometry.Line line,double tolerance = 0.001)
+    public static bool IsOnLine(Autodesk.DesignScript.Geometry.Point point, Autodesk.DesignScript.Geometry.Line line,
+        double tolerance = 0.001)
     {
-        Point3 point3 = new Point3(point.X, point.Y,point.Z);
+        Point3 point3 = new Point3(point.X, point.Y, point.Z);
         return point3.IsOnLine(line.ToGSharkType(), tolerance);
     }
 
@@ -69,13 +70,14 @@ public class Point
     /// <param name="tolerance">Default is use 1e-6</param>
     /// <returns>Returns true if point is on plane.</returns>
     [NodeCategory("Query")]
-    public static bool IsOnPlane(Autodesk.DesignScript.Geometry.Point point,Autodesk.DesignScript.Geometry.Plane plane,double tolerance = 0.001)
+    public static bool IsOnPlane(Autodesk.DesignScript.Geometry.Point point, Autodesk.DesignScript.Geometry.Plane plane,
+        double tolerance = 0.001)
     {
-        Point3 point3 = new Point3(point.X, point.Y,point.Z);
+        Point3 point3 = new Point3(point.X, point.Y, point.Z);
         return point3.IsOnPlane(plane.ToGSharkType(), tolerance);
     }
-    
-    
+
+
     /// <summary>
     ///  Deconstruct a point into its components
     /// </summary>
@@ -84,7 +86,7 @@ public class Point
     /// <returns name="Y">Y point</returns>
     /// <returns name="Z">Z point</returns>
     [MultiReturn("X", "Y", "Z")]
-    public static Dictionary<string,object?> Deconstruct(Autodesk.DesignScript.Geometry.Point point)
+    public static Dictionary<string, object?> Deconstruct(Autodesk.DesignScript.Geometry.Point point)
     {
         if (point == null) throw new ArgumentNullException(nameof(point));
         return new Dictionary<string, object?>
@@ -95,16 +97,31 @@ public class Point
         };
     }
 
-    
+
     //TODO : Got some bug from G-Shark Library
     // /// <summary>
     // /// Tests whether a point is inside, outside, or coincident with a polygon.
     // /// </summary>
     // /// <returns name="double">Returns -1 if point is outside the polygon, 0 if it is coincident with a polygon edge, or 1 if it is inside the polygon.</returns>
-    // public static double IsInPolyGon(Autodesk.DesignScript.Geometry.Point point,Autodesk.DesignScript.Geometry.Polygon polygon)
+    // public static double IsInPolygon(Autodesk.DesignScript.Geometry.Point point,Autodesk.DesignScript.Geometry.Polygon polygon)
     // {
     //     if (point == null) throw new ArgumentNullException(nameof(point));
     //     if (polygon == null) throw new ArgumentNullException(nameof(polygon));
     //    return point.ToGSharkType().InPolygon(polygon.ToGSharkType());
     // }
+
+    /// <summary>
+    /// Returns whether an input point is contained within the polygon. If the polygon is not planar then the point will be projected onto the best-fit plane and the containment will be computed using the projection of the polygon onto the best-fit plane. This will return a failed status if the polygon self-intersects.
+    /// </summary>
+    /// <param name="point">the point</param>
+    /// <param name="polygon">the polygon</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static bool IsInPolygon(Autodesk.DesignScript.Geometry.Point point,
+        Autodesk.DesignScript.Geometry.Polygon polygon)
+    {
+        if (point == null) throw new ArgumentNullException(nameof(point));
+        if (polygon == null) throw new ArgumentNullException(nameof(polygon));
+        return polygon.ContainmentTest(point);
+    }
 }
