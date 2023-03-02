a9901f07a9d0858f254391d06807cca7c654f6f9
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-03-02 20:28:03 +0800

    Add support duct type, duct system Type

---
+++ OpenMEP/Element/DuctType.cs
@@ -0,0 +1,48 @@
+﻿using OpenMEP.Helpers;
+using RevitServices.Persistence;
+
+namespace OpenMEP.Element;
+
+public class DuctType
+{
+    private DuctType()
+    {
+        
+    }
+    
+    /// <summary>
+    /// get pipe type by name
+    /// </summary>
+    /// <param name="typeName">type name of duct</param>
+    /// <returns name="ductType">type of duct</returns>
+    public static Revit.Elements.Element? GetDuctTypeByName(string typeName)
+    {
+        return GetAllDuctTypes().FirstOrDefault(x => x!.Name == typeName);
+    }
+    
+    /// <summary>
+    ///  return all duct types of the current document
+    /// </summary>
+    /// <returns name="ductTypes">All Duct Types</returns>
+    public static List<Revit.Elements.Element?> GetAllDuctTypes()
+    {
+        // Get all the pipe types in the current document
+        Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+        Autodesk.Revit.DB.FilteredElementCollector collector = new Autodesk.Revit.DB.FilteredElementCollector(doc);
+        Autodesk.Revit.DB.ElementClassFilter filter = new Autodesk.Revit.DB.ElementClassFilter(typeof(Autodesk.Revit.DB.Mechanical.DuctType));
+        Autodesk.Revit.DB.FilteredElementIterator iterator = collector.WherePasses(filter).GetElementIterator();
+        iterator.Reset();
+        List<Revit.Elements.Element?> pipeTypes = new List<Revit.Elements.Element?>();
+        while (iterator.MoveNext())
+        {
+            Autodesk.Revit.DB.Element element = iterator.Current!;
+            if (element is Autodesk.Revit.DB.Mechanical.DuctType ductType)
+            {
+                pipeTypes.Add(ductType.ToDynamoType());
+            }
+        }
+        return pipeTypes;
+    }
+
+    
+}
