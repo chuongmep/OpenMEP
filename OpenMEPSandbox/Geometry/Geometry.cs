1d2835303dc90af91965dbc5561cf47b1166eaf1
Chuong Ho <chuongpqvn@gmail.com>
2023-03-28 17:57:09 +0800

    improve docs

--- OpenMEPSandbox/Geometry/Geometry.cs
+++ OpenMEPSandbox/Geometry/Geometry.cs
@@ -7,18 +7,18 @@ public class Geometry
     }
 
     /// <summary>
-    /// Get Top Surface From Geometry
+    /// Returns the top surface of the provided geometry.
     /// </summary>
-    /// <param name="geometry">the geometry to get top of surfaces</param>
-    /// <returns name="surfaces">list top surface of geometry</returns>
+    /// <param name="geometry">The geometry to extract the top surface from.</param>
+    /// <returns name="surfaces">A list of the top surface(s) of the geometry.</returns>
     public static List<Autodesk.DesignScript.Geometry.Surface> TopSurfaces(
         Autodesk.DesignScript.Geometry.Geometry geometry)
     {
         List<Autodesk.DesignScript.Geometry.Surface> TopSurface = new List<Autodesk.DesignScript.Geometry.Surface>();
         Autodesk.DesignScript.Geometry.Geometry[] surfaces = geometry.Explode();
-        foreach (var geometry1 in surfaces)
+        foreach (var surface in surfaces)
         {
-            var g = (Autodesk.DesignScript.Geometry.Surface) geometry1;
+            var g = (Autodesk.DesignScript.Geometry.Surface) surface;
             double d = g.NormalAtParameter(0.5, 0.5).Z;
             if (d > 0.1)
             {
@@ -30,18 +30,18 @@ public class Geometry
     }
 
     /// <summary>
-    /// Return Bottom Surface from Geometry
+    /// Returns the bottom surface of the provided geometry.
     /// </summary>
-    /// <param name="geometry">the geometry get bottom surface</param>
-    /// <returns name="surfaces">list surface bottom of geometry</returns>
+    /// <param name="geometry">The geometry to extract the bottom surface from.</param>
+    /// <returns name="surfaces">A list of the bottom surface(s) of the geometry.</returns>
     public static List<Autodesk.DesignScript.Geometry.Surface> BottomSurfaces(
         Autodesk.DesignScript.Geometry.Geometry geometry)
     {
         List<Autodesk.DesignScript.Geometry.Surface> BottomSurface = new List<Autodesk.DesignScript.Geometry.Surface>();
         Autodesk.DesignScript.Geometry.Geometry[] surfaces = geometry.Explode();
-        foreach (var geometry1 in surfaces)
+        foreach (var surface in surfaces)
         {
-            var g = (Autodesk.DesignScript.Geometry.Surface) geometry1;
+            var g = (Autodesk.DesignScript.Geometry.Surface) surface;
             double d = g.NormalAtParameter(0.5, 0.5).Z;
             if (d < 0)
             {
@@ -53,19 +53,19 @@ public class Geometry
     }
 
     /// <summary>
-    /// Return Side Surface Vertical from Geometry
+    /// Returns a list of side surfaces that are vertical to the provided geometry.
     /// </summary>
-    /// <param name="geometry">the geometry to get side surface</param>
-    /// <returns name="surface">list surface from size</returns>
+    /// <param name="geometry">The geometry to extract side surfaces from.</param>
+    /// <returns name="surface">A list of side surfaces of the geometry.</returns>
     public static List<Autodesk.DesignScript.Geometry.Surface> SideSurface(
         Autodesk.DesignScript.Geometry.Geometry geometry)
     {
         List<Autodesk.DesignScript.Geometry.Surface> SideSurface = new List<Autodesk.DesignScript.Geometry.Surface>();
         Autodesk.DesignScript.Geometry.Geometry[] surfaces = geometry.Explode();
         // get all surface parallel to Z axis
-        foreach (var geometry1 in surfaces)
+        foreach (var surface in surfaces)
         {
-            var g = (Autodesk.DesignScript.Geometry.Surface) geometry1;
+            var g = (Autodesk.DesignScript.Geometry.Surface) surface;
             double d = g.NormalAtParameter(0.5, 0.5).Z;
             if (d > -0.1 && d < 0.1)
             {
