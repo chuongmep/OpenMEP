984b694c9c7f3f5d999ebfcce54892c68515763e
Chuong Ho <chuongpqvn@gmail.com>
2023-03-31 15:32:53 +0800

    add support shortest path

---
+++ OpenMEPSandbox/Algo/BellmanFord.cs
@@ -0,0 +1,188 @@
+﻿using Autodesk.DesignScript.Runtime;
+
+namespace OpenMEPSandbox.Algo;
+
+[IsVisibleInDynamoLibrary(false)]
+public class BellmanFord
+{
+    private readonly int _vertices;
+    private readonly int _edges;
+    private readonly List<(int, int, int)> _edgeList;
+
+    public BellmanFord(int vertices, int edges)
+    {
+        _vertices = vertices;
+        _edges = edges;
+        _edgeList = new List<(int, int, int)>();
+    }
+
+    /// <summary>
+    /// Adds a directed edge from the given source vertex to the given destination vertex with the given weight.
+    /// </summary>
+    /// <param name="source">The source vertex of the edge.</param>
+    /// <param name="destination">The destination vertex of the edge.</param>
+    /// <param name="weight">The weight of the edge.</param>
+    public void AddEdge(int source, int destination, int weight)
+    {
+        _edgeList.Add((source, destination, weight));
+    }
+
+    /// <summary>
+    /// Finds the shortest path from the given source vertex to the given destination vertex using the Bellman-Ford algorithm,
+    /// and returns an array of the vertices along the path in the order they are visited, as well as the total distance of the path.
+    /// If there is no path from the source to the destination, or if there is a negative weight cycle in the graph, returns null.
+    /// </summary>
+    /// <param name="source">The source vertex for the shortest path.</param>
+    /// <param name="destination">The destination vertex for the shortest path.</param>
+    /// <returns>An array of the vertices along the shortest path, in order, and the total distance of the path; or null if no such path exists.</returns>
+    public (List<int>, int) GetShortestPathAndDistance(int source, int destination)
+    {
+        int[] distance = new int[_vertices];
+        int[] predecessor = new int[_vertices];
+
+        for (int i = 0; i < _vertices; i++)
+        {
+            distance[i] = int.MaxValue;
+            predecessor[i] = -1;
+        }
+
+        distance[source] = 0;
+
+        for (int i = 1; i < _vertices; i++)
+        {
+            foreach ((int u, int v, int w) in _edgeList)
+            {
+                if (distance[u] != int.MaxValue && distance[u] + w < distance[v])
+                {
+                    distance[v] = distance[u] + w;
+                    predecessor[v] = u;
+                }
+            }
+        }
+
+        List<int> path = new List<int>();
+        int current = destination;
+        while (current != source)
+        {
+            path.Add(current);
+            current = predecessor[current];
+        }
+
+        path.Add(source);
+        path.Reverse();
+
+        return (path, distance[destination]);
+    }
+}
+
+[IsVisibleInDynamoLibrary(false)]
+public class BellmanFordDistance
+{
+    private int V; // number of vertices
+    private int E; // number of edges
+    private List<Edge> edges; // list of edges
+    private int[] distances; // array to store the distance from the source vertex to each vertex
+    private int[] predecessors; // array to store the predecessor of each vertex in the shortest path
+
+    /// <summary>
+    /// Create a new instance of the BellmanFordDistance class.
+    /// </summary>
+    /// <param name="V">number of vertices</param>
+    /// <param name="E">number of edges</param>
+    public BellmanFordDistance(int V, int E)
+    {
+        this.V = V;
+        this.E = E;
+        edges = new List<Edge>();
+        distances = new int[V];
+        predecessors = new int[V];
+    }
+
+    public void AddEdge(int src, int dest, int weight)
+    {
+        edges.Add(new Edge(src, dest, weight));
+    }
+
+    public void CalculateShortestPath(int src)
+    {
+        // initialize distances and predecessors
+        for (int i = 0; i < V; i++)
+        {
+            distances[i] = int.MaxValue;
+            predecessors[i] = -1;
+        }
+
+        distances[src] = 0;
+
+        // relax edges V-1 times
+        for (int i = 1; i < V; i++)
+        {
+            foreach (Edge edge in edges)
+            {
+                if (distances[edge.Src] != int.MaxValue && distances[edge.Src] + edge.Weight < distances[edge.Dest])
+                {
+                    distances[edge.Dest] = distances[edge.Src] + edge.Weight;
+                    predecessors[edge.Dest] = edge.Src;
+                }
+            }
+        }
+
+        // check for negative-weight cycles
+        foreach (Edge edge in edges)
+        {
+            if (distances[edge.Src] != int.MaxValue && distances[edge.Src] + edge.Weight < distances[edge.Dest])
+            {
+                throw new Exception("Graph contains negative-weight cycle");
+            }
+        }
+    }
+
+    public List<int> GetShortestPath(int dest)
+    {
+        List<int> path = new List<int>();
+        int curr = dest;
+        while (curr != -1)
+        {
+            path.Insert(0, curr);
+            curr = predecessors[curr];
+        }
+
+        return path;
+    }
+
+    public int GetDistance(int dest)
+    {
+        return distances[dest];
+    }
+
+    public Dictionary<int, Tuple<List<int>, int>> GetShortestPathsAndDistances(int src)
+    {
+        // initialize dictionary
+        Dictionary<int, Tuple<List<int>, int>> shortestPaths = new Dictionary<int, Tuple<List<int>, int>>();
+
+        // calculate shortest path and distance for each vertex
+        for (int i = 0; i < V; i++)
+        {
+            CalculateShortestPath(src);
+            List<int> path = GetShortestPath(i);
+            int distance = GetDistance(i);
+            shortestPaths.Add(i, new Tuple<List<int>, int>(path, distance));
+        }
+
+        return shortestPaths;
+    }
+
+    private class Edge
+    {
+        public int Src { get; }
+        public int Dest { get; }
+        public int Weight { get; }
+
+        public Edge(int src, int dest, int weight)
+        {
+            Src = src;
+            Dest = dest;
+            Weight = weight;
+        }
+    }
+}
--- OpenMEPSandbox/Geometry/Graph.cs
+++ OpenMEPSandbox/Geometry/Graph.cs
@@ -1,50 +1,162 @@
-﻿using OpenMEPSandbox.Algo;
+﻿using Autodesk.DesignScript.Runtime;
+using OpenMEPSandbox.Algo;
 
 namespace OpenMEPSandbox.Geometry;
-using Autodesk.DesignScript.Geometry;
+
 public class Graph
 {
     private Graph()
     {
         
     }
+
     /// <summary>
-    /// Adds an edge between two vertices in the graph, with a specified weight.
+    /// Creates a new `BellmanFord` instance with edges defined by the given source, target, and weight lists, and returns the instance.
+    /// The `v` and `e` parameters specify the number of vertices and edges in the graph, respectively.
+    /// The `source`, `target`, and `weight` lists define the source, destination, and weight of each edge, respectively.
     /// </summary>
-    /// <param name="sources">The starting vertex of the edge.</param>
-    /// <param name="destinations">The ending vertex of the edge.</param>
-    /// <param name="weight">The weight of the edge.</param>
-    /// <returns name="DijkstraGraph">DijkstraGraph</returns>
-    public static Algo.DijkstraGraph AddEdge(
-        List<Autodesk.DesignScript.Geometry.Point> sources,
-        List<Autodesk.DesignScript.Geometry.Point> destinations,List<double> weight)
+    /// <param name="sources">A list of source vertices for each edge.</param>
+    /// <param name="destinations">A list of destinations vertices for each edge.</param>
+    /// <param name="weights">A list of weights for each edge.</param>
+    /// <returns>A new `BellmanFord` instance with the specified edges.</returns>
+    /// <example>
+    /// ![](../OpenMEPPage/geometry/dyn/pic/Graph.AddEdge.png)
+    /// </example>
+    public static BellmanFord AddEdge(List<double> sources, List<double> destinations, List<double> weights)
     {
-        if (sources.Count != weight.Count)
-            throw new ArgumentNullException($"The number of points in u and weight must be equal to can crate a edge.");
-        if(sources.Count != destinations.Count)
+        //Get unique number in sources
+        var uniqueSources = sources.Distinct();
+        //Get unique number in targets
+        var uniqueTargets = destinations.Distinct();
+        //Get unique number in sources and targets
+        var vertex = uniqueSources.Union(uniqueTargets).Count();
+        var edge = sources.Count;
+        BellmanFord bf = new BellmanFord(vertex, edge);
+        //greatest common divisor of sources and targets
+        var gcd = GCD(sources, destinations);
+        // new sources and targets
+        for (int i = 0; i < edge; i++)
         {
-            throw new Exception($"The number of points in u and v must be equal to can crate a edge.");
+            sources[i] = sources[i] / gcd;
+            destinations[i] = destinations[i] / gcd;
         }
-        DijkstraGraph graph = new Algo.DijkstraGraph();
-        for(int i = 0; i < sources.Count; i++)
+        // add edge
+        for (int i = 0; i < edge; i++)
         {
-            graph.AddEdge(sources[i], destinations[i],weight[i]);
+            bf.AddEdge((int) sources[i], (int) destinations[i], (int) weights[i]);
         }
-        return graph;
+        return bf;
+    }
+    private static double GCD(List<double> sources, List<double> destinations)
+    {
+        // greatest common divisor of sources and targets
+        double gcd = 1;
+        for (int i = 0; i < sources.Count; i++)
+        {
+            gcd = GCD(sources[i], destinations[i]);
+        }
+        return gcd;
+    }
+    private static double GCD(double a, double b)
+    {
+        // greatest common divisor of sources and targets
+        if (a == 0)
+            return b;
+        return GCD(b % a, a);
+    }
+
+    /// <summary>
+    /// Get shortest path and distance from start node to end node By Bellman-Ford algorithm
+    /// </summary>
+    /// <param name="bellmanFord">a class define include edge,vertex added</param>
+    /// <param name="startNode">first location of node</param>
+    /// <param name="endNode">end location of node</param>
+    /// <returns name="shortestPath">the shortest path</returns>
+    /// <returns name="distance">the value distance shortest of path</returns>
+    /// <exception cref="Exception"></exception>
+    /// <example>
+    /// ![](../OpenMEPPage/geometry/dyn/pic/Graph.GetShortestPath.png)
+    /// </example>
+    [MultiReturn("shortestPath", "distance")]
+    public static Dictionary<string,object> GetShortestPath(BellmanFord bellmanFord, double startNode, double endNode)
+    {
+       
+        // check start node and end node is integer
+        if (Math.Abs(startNode - Math.Floor(startNode)) > 0.001 || Math.Abs(endNode - Math.Floor(endNode)) > 0.001)
+        {
+            throw new Exception("Start node and end node must be integer");
+        }
+        int start = (int) startNode;
+        int end = (int) endNode;
+        (List<int>, int) shortestPath = bellmanFord.GetShortestPathAndDistance(start, end);
+        return new Dictionary<string, object>
+        {
+            {"shortestPath", shortestPath.Item1},
+            {"distance", shortestPath.Item2}
+        };
+       
+    }
+
+    /// <summary>
+    /// Finds the shortest path from each source vertex to every other vertex in the graph using the Bellman-Ford algorithm,
+    /// and returns an array of tuples containing the distance and path to each vertex.
+    /// The `v` and `e` parameters specify the number of vertices and edges in the graph, respectively.
+    /// The `sources`, `targets`, and `weights` lists define the source, destination, and weight of each edge, respectively.
+    /// </summary>
+    /// <param name="from">value of node want start check</param>
+    /// <param name="sources">A list of source vertices for each edge.</param>
+    /// <param name="destinations">A list of target vertices for each edge.</param>
+    /// <param name="weights">A list of weights for each edge.</param>
+    /// <returns>
+    /// An array of tuples, where each tuple contains the distance and path from a source vertex to every other vertex in the graph.
+    /// If there is no path from a source to a destination vertex, or if there is a negative weight cycle in the graph,
+    /// the distance for that tuple will be `double.MaxValue` and the path will be an empty array.
+    /// </returns>
+    /// <example>
+    /// ![](../OpenMEPPage/geometry/dyn/pic/Graph.GetDistancesPathFromNode.png)
+    /// </example>
+    [MultiReturn("path", "distance")]
+    public static Dictionary<string,object> GetDistancesPathFromNode(double from,List<double> sources, List<double> destinations, List<double> weights)
+    {
+        //Get unique number in sources
+        var uniqueSources = sources.Distinct();
+        //Get unique number in destinations
+        var uniqueTargets = destinations.Distinct();
+        //Get unique number in sources and destinations to get vertex
+        var v = uniqueSources.Union(uniqueTargets).Count();
+        //Get number of edge
+        var e = sources.Count;
+        BellmanFordDistance bf = new BellmanFordDistance(v, e);
+        // add edge
+        for (int i = 0; i < e; i++)
+        {
+            bf.AddEdge((int) sources[i], (int) destinations[i], (int) weights[i]);
+        }
+        int fromInt = (int) from;
+        Dictionary<int, Tuple<List<int>, int>> shortestPaths = bf.GetShortestPathsAndDistances(fromInt);
+        List<int> vertexes = new List<int>();
+        List<List<int>> paths = new List<List<int>>();
+        List<int> distances = new List<int>();
+        foreach (KeyValuePair<int, Tuple<List<int>, int>> entry in shortestPaths)
+        {
+            int vertex = entry.Key;
+            List<int> path = entry.Value.Item1;
+            int distance = entry.Value.Item2;
+            // Console.WriteLine("Shortest path from vertex 0 to vertex " + vertex + ": " + string.Join(" -> ", path));
+            // Console.WriteLine("Distance: " + distance);
+            // Console.WriteLine();
+            vertexes.Add(vertex);
+            paths.Add(path);
+            distances.Add(distance);
+        }
+        return new Dictionary<string, object>()
+        {
+            {"vertex", vertexes},
+            {"path", paths},
+            {"distance", distances}
+
+        };
+        
     }
     
-    /// <summary>
-    /// Shortest path from start to end by Dijkstra algorithm.
-    /// </summary>
-    /// <param name="dijkstraGraph">graph init </param>
-    /// <param name="start"></param>
-    /// <param name="end"></param>
-    /// <returns anme="points">the shortest path</returns>
-    public static List<Autodesk.DesignScript.Geometry.Point>? ShortestPath(
-        Algo.DijkstraGraph dijkstraGraph,
-        Autodesk.DesignScript.Geometry.Point start,
-        Autodesk.DesignScript.Geometry.Point end)
-    {
-        return dijkstraGraph.DijkstraShortestPath(start, end);
-    }
 }
