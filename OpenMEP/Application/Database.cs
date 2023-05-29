21b09020b5e8e274eb474b74cbfa80002ee874b1
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-05-29 22:31:08 +0800

    support SnoopElements

---
+++ OpenMEP/Application/Database.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using RevitServices.Persistence;
+
+namespace OpenMEP.Application;
+
+public class Database
+{
+    private Database()
+    {
+    }
+
+    /// <summary>
+    /// Snoop Explore Elements
+    /// </summary>
+    /// <param name="elements">elements need to snoop</param>
+    /// <returns name="string">result</returns>
+    /// <example>
+    /// ![](../OpenMEPPage/application/dyn/pic/Database.SnoopElements.png)
+    /// [Database.SnoopElements.dyn](../OpenMEPPage/application/dyn/Database.SnoopElements.dyn)
+    /// </example>
+    public static string SnoopElements(List<global::Revit.Elements.Element> elements)
+    {
+        ICollection<ElementId> elementIds = elements.Select(x => x.InternalElement.Id).ToList();
+        DocumentManager.Instance.CurrentUIDocument.Selection.SetElementIds(elementIds);
+        RevitCommandId lookupCommandId = RevitCommandId.LookupCommandId("CustomCtrl_%CustomCtrl_%Add-Ins%Explorer%RevitDBExplorer.Command");
+        if(lookupCommandId == null)
+        {
+            TaskDialog.Show("Error", "Please install RevitDBExplorer");
+            Process.Start("https://github.com/NeVeSpl/RevitDBExplorer");
+            return "Please install RevitDBExplorer";
+        }
+        // execute the command
+        DocumentManager.Instance.CurrentUIApplication.PostCommand(lookupCommandId);
+        return "Success";
+    }
+}
