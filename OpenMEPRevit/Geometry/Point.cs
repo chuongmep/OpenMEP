26739dd986527bb066c66cc2cc68b0b8d825c48d
Chuong Ho <chuongpqvn@gmail.com>
2024-03-25 18:04:34 +0800

    support coordinate convert

--- OpenMEPRevit/Geometry/Point.cs
+++ OpenMEPRevit/Geometry/Point.cs
@@ -1,9 +1,35 @@
-﻿namespace OpenMEPRevit.Geometry;
+﻿using Autodesk.Revit.DB;
+using Revit.GeometryConversion;
+using RevitServices.Persistence;
+
+namespace OpenMEPRevit.Geometry;
 
 public class Point
 {
     private Point()
     {
         
+    }
+    /// <summary>
+    /// Converts a point from the origin coordinate system to the shared coordinate system in Revit.
+    /// </summary>
+    /// <param name="point">The point in the origin coordinate system to be converted.</param>
+    /// <returns>The converted point in the shared coordinate system.</returns>
+    public static Autodesk.DesignScript.Geometry.Point ConvertOriginToSharedCoordinate(Autodesk.DesignScript.Geometry.Point point)
+    {
+        Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+        XYZ ofPoint = doc.ActiveProjectLocation.GetTotalTransform().Inverse.OfPoint(point.ToXyz());
+        return ofPoint.ToPoint();
+    }
+    /// <summary>
+    /// Converts a point from the shared coordinate system to the origin coordinate system in Revit.
+    /// </summary>
+    /// <param name="point">The point in the shared coordinate system to be converted.</param>
+    /// <returns>The converted point in the origin coordinate system.</returns>
+    public static Autodesk.DesignScript.Geometry.Point ConvertSharedCoordinateToOrigin(Autodesk.DesignScript.Geometry.Point point)
+    {
+        Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+        XYZ ofPoint = doc.ActiveProjectLocation.GetTotalTransform().OfPoint(point.ToXyz());
+        return ofPoint.ToPoint();
     }
 }
